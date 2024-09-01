3ce2bfdbad48e651dfa170453bbb607c60fa2f77
mitch ci <mitchellci@outlook.com>
2024-09-01 15:30:55 +0000

    Fix: fix incorrect file name

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,7 +5,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('Project_4_Data.csv', index_col='date', parse_dates=True)
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
 
 # Clean data
 df = df.loc[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
